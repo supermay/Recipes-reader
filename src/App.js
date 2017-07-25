3e43472cf65c3827aa601891da147a6b3adc8913
supermay <wu_jiamei@outlook.com>
2017-07-25 09:58:22 +0200

    Install PropTypes and set validation

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
           <Title content="This is only for fun."/>
+          <Title />
         </p>
       </div>
     );
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,6 +1,10 @@
 import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 
 export default class Title extends PureComponent {
+  static propTypes = {
+    content: PropTypes.string.isRequired
+  }
   render() {
     return (
       <h1> {this.props.content } </h1>
