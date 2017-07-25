3d68cac36c57f2df1dc6ccd3912dc7ca37605e2a
supermay <wu_jiamei@outlook.com>
2017-07-25 10:02:07 +0200

    Update App.js show more contents

--- src/App.js
+++ src/App.js
@@ -12,9 +12,8 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
           <Title content="This is only for fun."/>
-          <Title />
+          <Title content="But waht to have for lunch???"/>
         </p>
       </div>
     );
