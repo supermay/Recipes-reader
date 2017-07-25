4658f8dcf084bd0d068e82242d3c78f21a8bfdb5
supermay <wu_jiamei@outlook.com>
2017-07-25 11:08:43 +0200

    Add Recipe Dummy Data in App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,37 @@ import './App.css';
 import Title from './components/Title'
 import RecipesContainer from './recipes/RecipesContainer'
 
+const recipes = [
+  {
+    title: 'Spanish Omelette',
+    summary: 'A traditional dish from Spanish cuisine called tortilla española or tortilla de patatas. It is an omelette made with eggs and potatoes, sometimes also with onion and/or chives or garlic; fried in oil and often served cold as an appetizer.',
+    vegan: false,
+    vegetarian: true,
+    pescatarian: false,
+  },
+  {
+    title: 'Paella',
+    summary: 'This is the dramatic seafood paella that looks stunning, with crustaceans and shellfish. You can vary the quantities of seafood and also use crab, crayfish, etc.',
+    vegan: false,
+    vegetarian: false,
+    pescatarian: true,
+  },
+  {
+    title: 'Spare ribs',
+    summary: 'Spare ribs are a variety of pork ribs cooked and eaten in various cuisines around the world. They are cut from the lower portion of the pig specifically the belly and breastbone, behind the shoulder, and include 11 to 13 long bones.',
+    vegan: false,
+    vegetarian: false,
+    pescatarian: false,
+  },
+  {
+    title: 'Agedashi Tofu',
+    summary: 'Agedashi Tofu is one of those magical dishes where a few simple ingredients come together in a harmonizing synergy that elevates the dish from humble to divine. It is made with blocks of soft tofu that are coated in a thin layer of potato starch before being lightly fried.',
+    vegan: true,
+    vegetarian: true,
+    pescatarian: false,
+  },
+]
+
 class App extends Component {
   render() {
     return (
