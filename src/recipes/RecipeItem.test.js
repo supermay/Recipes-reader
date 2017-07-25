f825ff7583fdd9dafd8f670420383ec269c78c5f
supermay <wu_jiamei@outlook.com>
2017-07-25 12:19:32 +0200

    Testing images for RecipeItem

--- src/recipes/RecipeItem.test.js
+++ src/recipes/RecipeItem.test.js
@@ -3,6 +3,9 @@ import chai, { expect } from 'chai'
 import { shallow } from 'enzyme'
 import chaiEnzyme from 'chai-enzyme'
 import RecipeItem from './RecipeItem'
+import Vegetarian from '../images/Vegetarian.svg'
+import Vegan from '../images/Vegan.svg'
+import Pescatarian from '../images/Pescatarian.svg'
 
 chai.use(chaiEnzyme())
 
@@ -24,5 +27,19 @@ describe('<RecipeItem />',() => {
   it('contains a the title',() => {
     expect(container.find('h1')).to.have.text(recipe.title)
   })
+
+  it('has a carrot image',() => {
+    expect(container.find('li')).to.contain(<img src={Vegetarian} alt="vegetarian" />)
+  })
+
+  it('doesn\'t have a corn image',() => {
+    expect(container.find('li')).not.to.contain(<img src={Vegan} alt="vegan" />)
+  })
+
+  it('doesn\'t have a fish image',() => {
+    expect(container.find('li')).not.to.contain(<img src={Pescatarian} alt="pescatarian" />)
+  })
+
+
 
 })
