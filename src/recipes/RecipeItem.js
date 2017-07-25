c9bb4352577ba757f9977fa98f51a36863fd6749
supermay <wu_jiamei@outlook.com>
2017-07-25 11:31:40 +0200

    Create RecipeItem.js

---
+++ src/recipes/RecipeItem.js
@@ -0,0 +1,21 @@
+import React, { PureComponent } from 'react'
+
+export default class RecipeItem extends PureComponent {
+  render() {
+    const { title, summary, vegan, vegetarian, pescatarian } = this.props
+
+    return(
+      <article className='recipe'>
+        <h1>{ Title }</h1>
+        <div>
+          <p>{ summary }</p>
+          <ul>
+            { vegan && <li><span role="img" aria-label="vegan"></span></li> }
+            { !vegan && vegetarian && <li><span role="img" aria-label="vegaratian"></span></li> }
+            { pescatarian && <li><span role="img" aria-label="pescatarian"></span></li> }
+          </ul>
+        </div>
+      </article>
+    )
+  }
+}
